208895eeb3a0128baee379eb0c6003487a3d032f
Luc010 <lucashurel@orange.fr>
2018-12-13 16:53:50 +0100

    ini

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
     <title></title>
   </head>
   <body>
-
+      <?php
+        $xml=simplexml_load_file("xml/source.xml") or die("Error: Cannot create object");
+      ?>
+      <p><?= $xml -> page[0] -> menu; ?></p>
+      <p><?= $xml -> page[0] -> title; ?></p>
+      <div><?= $xml -> page[0] -> content; ?></div>
   </body>
 </html>

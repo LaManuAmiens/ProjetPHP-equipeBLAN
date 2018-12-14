575e6d5b32406abf58f8885697a3fb06c06f383e
Paillier <aureliepaillier80@gmail.com>
2018-12-14 09:38:05 +0100

    first commit

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
     <title></title>
   </head>
   <body>
-
+    <?php 
+    // simplexml_load_string($ ) est une fonction qui sert à lire les XML data d'un dossier
+    $xml = simplexml_load_file("xml/source.xml") or die("Error: Cannot create object");
+    ?>
+    <p>
+      <?= $xml -> page[1] -> menu; ?>
+      <?= $xml -> page[1] -> title;?>
+      <?= $xml -> page[1] -> content;?>
+    </p>
   </body>
 </html>
